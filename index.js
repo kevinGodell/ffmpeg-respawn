4719a27604a270e5e56ef0dfc3fc8897551570f3
Kevin Godell <kevin.godell@gmail.com>
2018-02-15 23:21:53 -0600

    rearrange instance creation configuration

--- index.js
+++ index.js
@@ -21,111 +21,99 @@ class FfmpegRespawn extends EventEmitter {
     constructor(options) {
         super();
 
+        //options are required on instantiation
         if (!options) {
             throw new Error('Must pass a configuration object');
         }
-
-        //set defaults that will be passed to spawned ffmpeg, will update when looping through params
-        this._stdio = ['ignore', 'ignore', 'ignore', 'pipe'];
 
         if (!options.params || !Array.isArray(options.params) || options.params.length < 3) {
             throw new Error('Params error: must be an array with a minimum of 3 items.');
         }
 
-        //set params property
+        //set params property since it met the bare minimum
         this._params = options.params;
+
+        //set defaults that will be passed to spawned ffmpeg, will update when looping through params
+        this._stdio = ['ignore', 'ignore', 'ignore', 'ignore'];
+
+        let paramPipeCount = 0;
 
         //loop through params and configure pipes
         for (let i = 0; i < this._params.length; i++) {
             if (this._params[i] === '-i' && (this._params[i + 1] === 'pipe:0' || this._params[i + 1] === '-' || this._params[i + 1] === 'pipe:')) {
                 throw new Error('Params error: stdin/stdio[0]/pipe:0 not supported yet.');
-            } else if (this._params[i] === 'pipe:1' || ((this._params[i] === 'pipe:' || this._params[i] === '-') && this._params[i - 1] !== '-i')) {
-                this._stdio[1] = 'pipe';
-            } else if (this._params[i] === '-loglevel' && this._params[i + 1] !== 'quiet' && this._params[i + 1] !== '-8') {
-                this._stdio[2] = 'inherit';//will have to make this pipe if piping to writestream
-                //todo create writepipe to get and send out logs
-            } else if (this._params[i] === 'pipe:2') {
-                throw new Error('Params error: "pipe:2" is reserved for logging to console.');
-            } else if (this._params[i] === 'pipe:3') {
+            }
+            if (this._params[i] === 'pipe:2') {
+                throw new Error('Params error: "pipe:2" is reserved for ffmpeg logging to callback.');
+            }
+            if (this._params[i] === 'pipe:3') {
                 throw new Error('Params error: "pipe:3" is reserved for progress monitoring.');
+            }
+            //this._params[i] === '-loglevel' && this._params[i + 1] !== 'quiet' && this._params[i + 1] !== '-8'
+            if (this._params[i] === '-loglevel') {
+                throw new Error('Params error: "-loglevel" is not supported, set options.loglevel instead.');
+            }
+            if (this._params[i] === 'pipe:1' || this._params[i] === 'pipe:' || this._params[i] === '-') {
+                this._stdio[1] = 'pipe';
+                paramPipeCount++;
             } else {
                 const results = /pipe:(\d+)/.exec(this._params[i]);
                 if (results && results.index === 0) {
                     this._stdio[results[1]] = 'pipe';
+                    paramPipeCount++;
                 }
             }
         }
 
-        //test if any pipes were skipped based on params
+        if (paramPipeCount > 0 && (!options.pipes || !Array.isArray(options.pipes) || options.pipes.length !== paramPipeCount)) {
+            throw new Error(`Pipes error: options.pipes should contain a pipes/callback count of ${paramPipeCount}.`);
+        }
+
+        //create temp variable to hold array
+        const pipes = options.pipes;
+
+        //will be used to keep track of pipes passed to spawned ffmpeg process
+        this._stdioPipes = [];
+
+        //test if any pipes were skipped in options.params, and process options.pipes
         for (let i = 0; i < this._stdio.length; i++) {
             if (this._stdio[i] === undefined) {
                 throw new Error(`Params error: "pipe:${i}" was skipped.`);
             }
+            if (this._stdio[i] === 'pipe') {
+                let foundPipe = false;
+                for (let j = 0; j < pipes.length; j++) {
+                    if (pipes[j].stdioIndex === i) {
+                        const pipe = pipes[j];
+                        if (typeof pipe.destination === 'function' && pipe.destination.length > 0) {
+                            this._stdioPipes.push({stdioIndex: pipe.stdioIndex, destination: FfmpegRespawn._createWritable(pipe.destination)});
+                        } else if (pipe.destination instanceof Writable) {
+                            this._stdioPipes.push(pipe);
+                        } else {
+                            throw new Error(`Destination: "${pipe.destination}" must be a stream instance of Writable, Duplex, or Transform or a function that can receive a single param.`);
+                        }
+                        foundPipe = true;
+                    }
+                }
+                if (!foundPipe) {
+                    throw new Error(`Pipes error: options.pipes did not have a matching pipe or callback for pipe:${i}.`);
+                }
+            }
         }
+
+        //create and add the progress pipe to our stdioPipes array
+        this._stdioPipes.push({stdioIndex: 3, destination: FfmpegRespawn._createWritable(this._onProgress.bind(this))});
 
         //add the progress pipe to front of params array
         this._params.unshift(...['-progress', 'pipe:3']);
 
-        //will be used to keep track of pipes passed to spawned process
-        this._pipes = [];
+        //add 'pipe' to array that will activate stdio[3] from ffmpeg
+        this._stdio[3] = 'pipe';
 
-        //create writable pipe used for reading progress data
-        const onProgress = this._onProgress.bind(this);
-        const progressPipe = new Writable({
-            write(chunk, encoding, callback) {
-                const array = chunk.toString().split('\n').slice(0, -1);
-                const object = {};
-                for (let i = 0; i < array.length; i++) {
-                    const tempArr = array[i].split('=');
-                    object[tempArr[0]] = tempArr[1].trimLeft();
-                }
-                onProgress(object);
-                callback();
-            }
-        });
-
-        /*if (options.logCallback && typeof options.logCallback === 'function') {
-            this._logCallback = options.logCallback;
-        }
-
-        this._logCallback('hello');
-
-        const logCallback = this._logCallback.bind(this);
-        const logPipe = new Writable({
-            write(chunk, encoding, callback) {
-                //console.log(chunk.toString());
-                logCallback(chunk.toString());
-                callback();
-            }
-        });
-        this._pipes.push({stdioIndex: 2, destination: logPipe});*/
-
-        //add the progress pipe to our pipes array
-        this._pipes.push({stdioIndex: 3, destination: progressPipe});
-
-        //check pipes to see if necessary pipe:i's have been passed in params
-        if (options.pipes && Array.isArray(options.pipes) && options.pipes.length) {
-            for (let i = 0; i < options.pipes.length; i++) {
-                const pipe = options.pipes[i];
-                const stdioIndex = pipe.stdioIndex;
-                const destination = pipe.destination;
-                //check that it is writable, will include duplex and transform since they are inherited from writable
-                if(!(destination instanceof Writable)) {
-                    throw new Error(`Destination: "${destination}" must be a stream instance of Writable, Duplex, or Transform.`);
-                }
-                if (stdioIndex === 0) {
-                    throw new Error('Pipes error: stdin/stdio[0]/pipe:0 not supported yet');
-                }
-                if (stdioIndex === 2 || stdioIndex === 3) {
-                    throw new Error(`Pipes error: "pipe:${stdioIndex}" is reserved. Use pipe:1,pipe:4, or higher`);
-                }
-                if (this._stdio[stdioIndex] === 'pipe') {
-                    this._pipes.push(pipe);
-                } else {
-                    throw new Error(`Params/Pipes mismatch: pipe:${stdioIndex} not found in params.`);
-                }
-            }
-        }
+        //todo move loglevel to options
+        //if (options.loglevel) {
+        //console.log(options.loglevel);
+        //}
 
         //optional, path to ffmpeg
         if (options.path) {
@@ -134,12 +122,12 @@ class FfmpegRespawn extends EventEmitter {
             this._path = 'ffmpeg';
         }
 
-        //optional, function to be called when internal ffmpeg process exits
+        //optional, function to be called when internal ffmpeg process exits, but before it is respawned
         if (options.exitCallback && typeof options.exitCallback === 'function') {
             this._exitCallback = options.exitCallback;
         }
 
-        //configure time that passes without progress to trigger ffmpeg to be killed
+        //configure number of seconds that passes without progress before killing ffmpeg process
         const killAfterStall = parseInt(options.killAfterStall);
         if (isNaN(killAfterStall) || killAfterStall < 10) {
             this._killAfterStall = 10000;
@@ -149,7 +137,7 @@ class FfmpegRespawn extends EventEmitter {
             this._killAfterStall = killAfterStall * 1000;
         }
 
-        //configure time to wait before re spawning ffmpeg after exit
+        //configure time to wait before re spawning ffmpeg after exiting
         const spawnAfterExit = parseInt(options.spawnAfterExit);
         if (isNaN(spawnAfterExit) || spawnAfterExit < 2) {
             this._spawnAfterExit = 2000;
@@ -159,6 +147,7 @@ class FfmpegRespawn extends EventEmitter {
             this._spawnAfterExit = spawnAfterExit * 1000;
         }
 
+        //set number of failed attempts to respawn, number is reset if progress occurs
         const reSpawnLimit = parseInt(options.reSpawnLimit);
         if (isNaN(reSpawnLimit) || reSpawnLimit < 1) {
             this._reSpawnLimit = 1;
@@ -168,10 +157,13 @@ class FfmpegRespawn extends EventEmitter {
             this._reSpawnLimit = reSpawnLimit;
         }
 
-        //todo move loglevel to options
-        //if (options.loglevel) {
-            //console.log(options.loglevel);
-        //}
+        //output some details if in development
+        if (process.env.NODE_ENV === 'development') {
+            console.log(this._stdio);
+            console.log(this._stdioPipes);
+            console.log(this._params);
+        }
+
         return this;// not needed
         //todo check stdin for readable stream
     }
@@ -231,8 +223,8 @@ class FfmpegRespawn extends EventEmitter {
             throw error;
         });
         this._ffmpeg.once('exit', this._onExit.bind(this));
-        for (let i = 0; i < this._pipes.length; i++) {
-            this._ffmpeg.stdio[this._pipes[i].stdioIndex].pipe(this._pipes[i].destination, {end: false});
+        for (let i = 0; i < this._stdioPipes.length; i++) {
+            this._ffmpeg.stdio[this._stdioPipes[i].stdioIndex].pipe(this._stdioPipes[i].destination, {end: false});
         }
         this._startStallTimer();
     }
@@ -244,8 +236,8 @@ class FfmpegRespawn extends EventEmitter {
     _kill() {
         if (this._ffmpeg) {
             if (this._ffmpeg.kill(0)) {
-                for (let i = 0; i < this._pipes.length; i++) {
-                    this._ffmpeg.stdio[this._pipes[i].stdioIndex].unpipe(this._pipes[i].destination);
+                for (let i = 0; i < this._stdioPipes.length; i++) {
+                    this._ffmpeg.stdio[this._stdioPipes[i].stdioIndex].unpipe(this._stdioPipes[i].destination);
                 }
                 this._ffmpeg.kill('SIGHUP');//SIGTERM, SIGINT
             }
@@ -283,7 +275,13 @@ class FfmpegRespawn extends EventEmitter {
      * @param object
      * @private
      */
-    _onProgress(object) {
+    _onProgress(chunk) {
+        const array = chunk.toString().split('\n').slice(0, -1);
+        const object = {};
+        for (let i = 0; i < array.length; i++) {
+            const tempArr = array[i].split('=');
+            object[tempArr[0]] = tempArr[1].trimLeft();
+        }
         if (object.progress === 'continue') {
             this._startStallTimer();
             this._exitCounter = 0;
@@ -352,6 +350,21 @@ class FfmpegRespawn extends EventEmitter {
             clearTimeout(this._spawnTimer);
             delete this._spawnTimer;
         }
+    }
+
+    /**
+     *
+     * @param destination
+     * @return {"stream".internal.Writable}
+     * @private
+     */
+    static _createWritable(destination) {
+        return new Writable({
+            write(chunk, encoding, callback) {
+                destination(chunk);
+                callback();
+            }
+        });
     }
 }
 
